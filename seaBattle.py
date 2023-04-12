b49101e4f1291b5adaad7df32322acdb7e21364b
YanvarD <127998563+YanvarD@users.noreply.github.com>
2023-04-12 13:24:38 +0300

    game complited

--- seaBattle.py
+++ seaBattle.py
@@ -136,6 +136,9 @@ class Board:
     def bigin(self):
         self.busy = []
 
+    def defeat(self):
+        return self.count == len(self.ships)
+
 class Player:
     def __init__(self, board, enemy):
         self.board = board
@@ -176,6 +179,16 @@ class User(Player):
             x,y = int(x), int(y)
             return Dot(x-1,y-1)
 class Game:
+    def __init__(self, size = 6) -> None:
+        self.size = size
+        pl = self.random_board()
+        co = self.random_board()
+        co.hid = True
+
+        self.ai = AI(co, pl)
+
+        self.user = User(pl, co)
+
     def try_board(self):
         lens = [3,2,2,1,1,1,1]
         board = Board(size=self.size)
@@ -190,9 +203,55 @@ class Game:
                     break
                 except BoardWrongShipException:
                     pass
-        board.begin()
+        board.bigin()
+        return board
+    def random_board(self):
+        board = None
+        while board is None:
+            board = self.try_board()
         return board
     
+    def greet(self):
+        print("------------------------------")
+        print("    Приветствуем в игре  ")
+        print("         МОРСКОЙ БОЙ     ")
+        print("------------------------------")
+        print("     Формат ввода: х, у")
+
+    def loop(self):
+        num = 0
+        while True:
+            print("-" * 20)
+            print("Доска пользовател:я:")
+            print(self.user.board)
+            print("-" * 20)
+            print("Доска компьютера:")
+            print(self.ai.board)
+            print("-" * 20)
+
+            if num %2 == 0:
+                print("Ходит пользователь ")
+                repeat = self.user.move()
+            else:
+                print("Ходит компьютер")
+                repeat = self.ai.move()
+            
+            if repeat:
+                num -= 1
+            
+            if self.ai.board.defeat():
+                print("-" * 20)
+                print("Пользователь выиграл")
+                break
+            if self.user.board.defeat():
+                print("-" * 20)
+                print("Выиграл ai")
+                break
+            num += 1
+    def start(self):
+        self.greet()
+        self.loop()
+
 g = Game()
-g.size = 6
-print(g.try_board())
+g.start()
+
